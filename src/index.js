a6a5a01956b380ac8742795e02b08951b0b90d09
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-06-13 14:38:10 +0530

    Add projects section html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import "dracula-ui/styles/dracula-ui.css";
 import "./styles/style.css";
 
+const arrayOfTodo = [];
+
+const todoFactory = (title, desc, dueDate, priority) =>{
+  return { title, desc, dueDate, priority};
+}
+
+const newTodo = todoFactory("do this", "now", "tomorow", 1);
